c4d475fbf04fa0fd313c46e0cf9d49c892d6c2d0
Nii Botchway <ishmoto4u@gmail.com>
2021-01-01 14:06:43 +0000

    Initial commit

---
+++ app/src/main/java/com/ananseLabs/rickandmorty/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ananseLabs.rickandmorty.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ananseLabs.rickandmorty.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
